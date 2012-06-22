f2a17c62d6fbffb91b8e53b902811c95a62979f9
Jon Suderman <suderman@gmail.com>
2012-06-22 12:40:21 -0600

    Update master

--- installion.rb
+++ installion.rb
@@ -3,6 +3,6 @@ require 'formula'
 class Installion < GithubGistFormula
   url 'https://raw.github.com/suderman/installion/master/installion'
   homepage 'https://github.com/suderman/installion'
-  md5 'b92e835e120b4b3762b058944a885030'
-  version '0.1'
+  md5 'cf36c64ebd352935f08ceeae4a9949c0'
+  version '0.2'
 end
