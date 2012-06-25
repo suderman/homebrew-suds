c50f0e5985c0c5566993ac6272fd61dca37c6dd3
Jon Suderman <suderman@gmail.com>
2012-06-25 11:45:13 -0600

    installion… without the bugs!

--- installion.rb
+++ installion.rb
@@ -3,6 +3,6 @@ require 'formula'
 class Installion < GithubGistFormula
   url 'https://raw.github.com/suderman/installion/master/installion'
   homepage 'https://github.com/suderman/installion'
-  md5 '20ff3e5b4f69788be6965b3830dae015'
-  version '0.4'
+  md5 '6293ef90e4fe51d46f424d977bdd7fe3'
+  version '0.5'
 end
