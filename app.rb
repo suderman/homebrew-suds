1affe3bc0712da4ce1e85e5b72e26974945befdb
Jon Suderman <suderman@gmail.com>
2012-06-28 17:37:21 -0600

    Rename 'installion' to 'app' ...just because it's shorter

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class App < GithubGistFormula
+  url 'https://raw.github.com/suderman/app/master/app'
+  homepage 'https://github.com/suderman/app'
+  md5 'bc7658ef4442775a9a9079f8ef5fea67'
+  version '0.5'
+end
--- installion.rb
+++
@@ -1,8 +0,0 @@
-require 'formula'
-
-class Installion < GithubGistFormula
-  url 'https://raw.github.com/suderman/installion/master/installion'
-  homepage 'https://github.com/suderman/installion'
-  md5 '0a98171fb02165d9f0eda120c1f7aa96'
-  version '0.5'
-end
