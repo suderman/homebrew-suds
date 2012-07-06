d68988a2669ca01d57e092a30ec76b0ea5d892d6
Jon Suderman <suderman@gmail.com>
2012-07-06 13:51:05 -0600

    Add appstack formula

---
+++ appstack.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class AppStack < GithubGistFormula
+  url 'https://raw.github.com/suderman/appstack/master/appstack'
+  homepage 'https://github.com/suderman/appstack'
+  md5 '9d7e7f56b40ecc8104ba240c262d8552'
+  version '0.1'
+end
