8a1e76fc9ff167326d00153b06899c0c1ca34c79
Jon Suderman <suderman@gmail.com>
2012-07-27 01:51:15 -0600

    Update appstack.rb

--- appstack.rb
+++ appstack.rb
@@ -3,6 +3,6 @@ require 'formula'
 class Appstack < GithubGistFormula
   url 'https://raw.github.com/suderman/appstack/master/appstack'
   homepage 'https://github.com/suderman/appstack'
-  md5 'adf266127844c7a0f66085dc896b4a38'
-  version '0.3'
+  md5 'c1e8c27b6ec403ee027601cd55b7447f'
+  version '0.4'
 end
