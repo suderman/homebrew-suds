c86339d8c08c1a8b227d18927263860582c0a31a
Jon Suderman <suderman@gmail.com>
2012-07-26 18:49:24 -0600

    Symlink forumula

---
+++ symlink.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class Symlink < GithubGistFormula
+  url 'https://raw.github.com/suderman/symlink/master/symlink'
+  homepage 'https://github.com/suderman/symlink'
+  md5 '86ad785761e3357ffef140887f3b577a'
+  version '0.1'
+end
