3eeabe702c4ccba938acdb4785b0718c8b5159bd
Jon Suderman <suderman@gmail.com>
2012-07-26 19:18:20 -0600

    dockutil

---
+++ dockutil.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class Dockutil < GithubGistFormula
+  url 'https://raw.github.com/kcrawford/dockutil/master/scripts/dockutil'
+  homepage 'https://github.com/kcrawford/dockutil'
+  md5 'ac4cdcdc0fc3073dde060c60844e649b'
+  version '1.1.2'
+end
