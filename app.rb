b898a5d34db8f7f7985e1d8472760ed7be1366e5
Jon Suderman <suderman@gmail.com>
2012-07-26 18:26:19 -0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,6 +3,6 @@ require 'formula'
 class App < GithubGistFormula
   url 'https://raw.github.com/suderman/app/master/app'
   homepage 'https://github.com/suderman/app'
-  md5 'bc7658ef4442775a9a9079f8ef5fea67'
-  version '0.5'
+  md5 'b32873034f258c0d38b00ed222bf2b23'
+  version '0.6'
 end
