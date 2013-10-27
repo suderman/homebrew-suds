ae8aba2df32b44336e9da1c8500ece8186b85faa
Jon Suderman <suderman@gmail.com>
2013-10-27 01:37:36 -0600

    MD5 was wrong

--- dockutil.rb
+++ dockutil.rb
@@ -3,6 +3,6 @@ require 'formula'
 class Dockutil < GithubGistFormula
   url 'https://raw.github.com/kcrawford/dockutil/master/scripts/dockutil'
   homepage 'https://github.com/kcrawford/dockutil'
-  md5 '7f47068d5d7c0568ceb53686e39bc1d7'
+  md5 'cef3cebc8dc6db7326dd1821d279ca36'
   version '1.1.3'
 end
